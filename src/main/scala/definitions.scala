66eb9d8602c384c452ea07768bd8cc6f7a310cb8
3amo-magdy <amoramagdymahmoud@gmail.com>
2022-05-26 20:21:37 +0200

    check definitions (state should be a 2d array of strings)

---
+++ src/main/scala/definitions.scala
@@ -0,0 +1,26 @@
+import definitions.status.status
+import scalafx.scene.Node
+
+object definitions{
+  def BOARDWIDTH: Int = 600
+  type input = String
+  type turn = Int
+  type state = Array[Array[String]]
+  type drawable = List[Node]
+  type drawer = state =>drawable
+  type controller = (state,input,turn)=>(state,status,String)
+
+  object status extends Enumeration{
+    type status = Value
+    //for move:
+    val Invalid: status.Value = Value("Invalid")
+    //for game and turns:
+    //running
+    val Player_0_turn: status.Value = Value("First Player's turn")
+    val Player_1_turn: status.Value = Value("Second Player's turn")
+    //ended
+    val Player_0_won: status.Value = Value("First Player Won")
+    val Player_1_won: status.Value = Value("Second Player Won")
+    val Draw: status.Value = Value("Draw")
+  }
+}
