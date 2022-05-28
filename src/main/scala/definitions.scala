3619a304824d6fa01937a16b61296c96584f9d9d
3amo-magdy <amoramagdymahmoud@gmail.com>
2022-05-28 04:10:55 +0200

    xo glow effect

--- src/main/scala/definitions.scala
+++ src/main/scala/definitions.scala
@@ -13,14 +13,14 @@ object definitions{
   object status extends Enumeration{
     type status = Value
     //for move:
-    val Invalid: status.Value = Value("Invalid")
+    val Invalid: status.Value = Value(">>Invalid")
     //for game and turns
     //running
-    val Player_0_turn: status.Value = Value("First Player's turn")
-    val Player_1_turn: status.Value = Value("Second Player's turn")
+    val Player_0_turn: status.Value = Value(">>First Player's turn:")
+    val Player_1_turn: status.Value = Value(">>Second Player's turn:")
     //ended
-    val Player_0_won: status.Value = Value("First Player Won")
-    val Player_1_won: status.Value = Value("Second Player Won")
-    val Draw: status.Value = Value("Draw")
+    val Player_0_won: status.Value = Value(">>First Player Won!")
+    val Player_1_won: status.Value = Value(">>Second Player Won!")
+    val Draw: status.Value = Value(">>Draw")
   }
 }
