a5b84788bd8dcd80e900baa20c1e257892932deb
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-05-28 14:31:31 +0200

    promotion

--- src/main/scala/Chess.scala
+++ src/main/scala/Chess.scala
@@ -33,23 +33,24 @@ object Chess{
     val Y = 8 - (y/(chess_BOARDWIDTH/8)).toInt
     s"${X.toString}${Y}"
   }
-  def promotedpiece (turn : Int) : String = {
+  def promotedpiece (turn : Int , piece : String) : Boolean = {
     println("Your pawn can be promoted choose a piece")
-    val piece = scala.io.StdIn.readLine()
+
     println(piece)
+    println(turn)
     if(turn == 0){
       if(piece!="r" && piece!="q" && piece!="n" && piece!="b"){
         println("not valid piece")
-        promotedpiece(turn)
+        false
       }else{
-        piece
+        true
       }
     }else{
       if(piece!="R" && piece!="Q" && piece!="N" && piece!="B"){
         println("not valid piece")
-        promotedpiece(turn)
+        false
       }else{
-        piece
+       true
       }
     }
   }
@@ -73,7 +74,8 @@ object Chess{
     }
   }
 
-  def checkPromotion (turn : Int , board : state) : state = {
+  def Promotion (turn : Int , board : state , piece : String) : state = {
+    println(" func promotion >>>")
     var y_pos = -1
 
     if(turn == 0){      // white turn
@@ -84,7 +86,7 @@ object Chess{
       }
       println(y_pos)
       if(y_pos != -1) {
-        val piece = promotedpiece(turn)
+       // val piece = promotedpiece(turn)
         println("your piece is >> " + piece)
         board(0)(y_pos) = piece
       }
@@ -99,7 +101,7 @@ object Chess{
 
       println(y_pos)
       if(y_pos != -1){
-        val piece = promotedpiece(turn)
+       // val piece = promotedpiece(turn)
         println("your piece is >> " + piece)
         board(7)(y_pos) = piece
       }
@@ -475,6 +477,25 @@ object Chess{
     var index= new Array[Int](4);
     println(move)
     var flag :Boolean=false;
+    var promotion :Boolean = false;
+
+    println(canPrometed(turn,state))
+    println(promotedpiece(turn, move))
+
+    if(canPrometed(turn, state) && promotedpiece(turn, move)){
+      println("Promation validation")
+      Promotion(turn,state,move)
+      flag = true
+      promotion = true
+
+//      (state,if( flag ) if(turn%2==0)status.Player_0_turn else status.Player_1_turn else status.Invalid,
+//        if(!flag && !promotion) "Invalid move" else if (promotion) "Promotion" else "")
+    }
+
+    if(flag && promotion){
+      println("vvvvvvvvvvvv")
+      (state, if(turn%2==0)status.Player_1_turn else status.Player_0_turn ,"After Promotion" )
+    }
 
     /*
     steps >> 1) validate input  "tmam an l7rka gwa al board w anha kmlaa"
@@ -494,12 +515,16 @@ object Chess{
       if(generalValidation(index,turn,state) && moveValidation(index,turn,state)){
         println("validatiiiion done")
         moveGenerate(index,state)
-
-        if(canPrometed(turn , state)){
-          canPrometed(turn,state)
-        }
-        
         flag = true
+
+        if(canPrometed(turn , state)) {
+          promotion = true;
+          flag = false
+          (state,if(flag ) if(turn%2==0)status.Player_1_turn else status.Player_0_turn else status.Invalid,
+            if(!flag && !promotion) "Invalid move" else if (promotion) "Promotion" else "")
+        }
+
+
       }else{
         println("not valid")
         flag = false
@@ -510,7 +535,8 @@ object Chess{
       flag = false
     }
 
-    (state,if(flag) if(turn%2==0)status.Player_1_turn else status.Player_0_turn else status.Invalid,if(!flag) "Invalid move" else "")
+
+
   }
 
   /*
